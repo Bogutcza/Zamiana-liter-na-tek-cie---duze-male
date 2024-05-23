6ade6f2ebc24cbd905696c0ddf9a27aaaa8a0c11
Kacper <250946@edu.p.lodz.pl>
2024-05-23 17:08:53 +0200

    add_text

--- text.h
+++ text.h
@@ -4,5 +4,11 @@
 
 #ifndef UNTITLED3_TEXT_H
 #define UNTITLED3_TEXT_H
+void enterText(char *text);
+void printText(char *text);
+void clear(void);
+int count(char *text);
+void bigLetters(char *string);
+void smallLetters(char *string);
 
 #endif //UNTITLED3_TEXT_H
